ade60708e0b2b868673dd3ee6da4facfdfed235c
tanishsindhu <122311450+tanishsindhu@users.noreply.github.com>
2023-02-13 22:20:40 -0500

    Create Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  * @author Gulsher Feb 2023
- * @author Jivesh Malhotra Feb 2023
  * @author Chao Yen Chou Feb 2023
+ * @author Tanish Sindhu 
  */
 public abstract class Game {
 
